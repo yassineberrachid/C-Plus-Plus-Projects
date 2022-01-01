24d8bea7f4d7b380b251d8ffe8366a8244754646
yassineberrachid <95399226+yassineberrachid@users.noreply.github.com>
2022-01-01 20:08:35 +0100

    Update Shape.cpp

--- ShapesDataBase-LinkedLists/Shape.cpp
+++ ShapesDataBase-LinkedLists/Shape.cpp
@@ -1,21 +1,5 @@
-//
-//  Shape.cpp
-//  Lab4
-//
-//  Created by Tarek Abdelrahman on 2020-10-25.
-//  Copyright © 2020 Tarek Abdelrahman.
-//
-//  Permission is hereby granted to use this code in ECE244 at
-//  the University of Toronto. It is prohibited to distribute
-//  this code, either publicly or to third parties.
-
-//  ECE244 Student:
-//  Write the implementation (and only the implementation) of the Shape class 
-
 #include "Shape.h"
 #include <iostream>
-
-
 
 
 Shape::Shape(string n, string t, int x_loc, int y_loc, int x_sz, int y_sz){
