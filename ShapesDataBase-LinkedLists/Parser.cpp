ec2c1758931def6052a96265b994ab8fcff05bcf
yassineberrachid <95399226+yassineberrachid@users.noreply.github.com>
2022-01-01 20:05:38 +0100

    Update Parser.cpp

--- ShapesDataBase-LinkedLists/Parser.cpp
+++ ShapesDataBase-LinkedLists/Parser.cpp
@@ -1,14 +1,3 @@
-//
-//  parser.cpp
-//  Lab4
-//
-//  Created by Tarek Abdelrahman on 2020-10-25.
-//  Copyright © 2020 Tarek Abdelrahman.
-//
-//  Permission is hereby granted to use this code in ECE244 at
-//  the University of Toronto. It is prohibited to distribute
-//  this code, either publicly or to third parties.
-
 
 #include <iostream>
 #include <sstream>
