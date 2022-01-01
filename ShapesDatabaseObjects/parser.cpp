ac32fa16842378fda9bed0b533a3264903496aee
yassineberrachid <95399226+yassineberrachid@users.noreply.github.com>
2022-01-01 20:12:49 +0100

    Update parser.cpp

--- ShapesDatabaseObjects/parser.cpp
+++ ShapesDatabaseObjects/parser.cpp
@@ -1,16 +1,3 @@
-//
-//  parser.cpp
-//  lab3
-//
-//  Modified by Tarek Abdelrahman on 2020-10-04.
-//  Created by Tarek Abdelrahman on 2018-08-25.
-//  Copyright © 2018-2020 Tarek Abdelrahman.
-//
-//  Permission is hereby granted to use this code in ECE244 at
-//  the University of Toronto. It is prohibited to distribute
-//  this code, either publicly or to third parties.
-
-// If you already
 #include <iostream>
 #include <sstream>
 #include <string>
