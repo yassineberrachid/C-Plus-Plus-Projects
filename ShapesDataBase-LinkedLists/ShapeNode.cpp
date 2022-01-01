9d8cd7c218d2c982c110943e064f8ee18d23ee7b
yassineberrachid <95399226+yassineberrachid@users.noreply.github.com>
2022-01-01 20:11:08 +0100

    Update ShapeNode.cpp

--- ShapesDataBase-LinkedLists/ShapeNode.cpp
+++ ShapesDataBase-LinkedLists/ShapeNode.cpp
@@ -1,17 +1,3 @@
-//
-//  ShapeNode.cpp
-//  Lab4
-//
-//  Created by Tarek Abdelrahman on 2020-10-25.
-//  Copyright © 2020 Tarek Abdelrahman.
-//
-//  Permission is hereby granted to use this code in ECE244 at
-//  the University of Toronto. It is prohibited to distribute
-//  this code, either publicly or to third parties.
-
-//  ECE244 Student:
-//  Write the implementation (and only the implementation) of the ShapeNode class below
-
 #include "Shape.h"
 #include "ShapeNode.h"
 using namespace std;
